a5968f6f120ed62bbad86aa4ac7d1aad79c0fd61
yannick grenzinger <yannick.grenzinger@gmail.com>
2019-09-10 17:41:35 +0200

    adding react basic starter

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,19 @@ module Main where
 
 import Prelude
 
+import Components.App (app)
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Exception (throw)
+import React.Basic.DOM (render)
+import Web.DOM.NonElementParentNode (getElementById)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
+import Web.HTML.Window (document)
 
 main :: Effect Unit
 main = do
-  log "Hello sailor!"
+  root <- getElementById "root" =<< (map toNonElementParentNode $ document =<< window)
+  case root of
+    Nothing -> throw "Root element not found."
+    Just r  -> render app r
